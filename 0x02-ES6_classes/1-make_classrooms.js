efab8cdbc60d4bfd9ce505c956695a6941c245df
Omar-Algassim <omarggg2@gamil.com>
2024-06-27 15:38:52 +0300

    linint fix

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -2,10 +2,10 @@
 import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
-  const num = [19, 20, 34]
+  const num = [19, 20, 34];
   const rooms = [];
   for (let i = 0; i < num.length; i += 1) {
     rooms.push(new ClassRoom(num[i]));
   }
   return rooms;
-};
+}
