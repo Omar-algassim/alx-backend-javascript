083585f729d28e12f22a1ed917ab8879202634b4
Omar-Algassim <omarggg2@gamil.com>
2024-06-27 15:36:00 +0300

    add defult to export function

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 import ClassRoom from './0-classroom';
 
-export function initializeRooms() {
+export default function initializeRooms() {
   const num = [19, 20, 34]
   const rooms = [];
-  for (let i = 0; i < num.length(); i += 1) {
+  for (let i = 0; i < num.length; i += 1) {
     rooms.push(new ClassRoom(num[i]));
   }
   return rooms;
