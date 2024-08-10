146668d223ff06147b82f48bb38de4d12e5cfc0b
Omar-Algassim <omarggg2@gamil.com>
2024-08-10 18:42:02 +0300

    use then and finally

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,16 +1,8 @@
 #!/usr/bin/node
 
-export default function getFullResponseFromAPI(success) {
-  return (new Promise((resolve, reject) => {
-    if (success) {
-      resolve({
-        status: 200,
-        body: 'Success',
-      });
-      console.log('Got a response from the API');
-    } else {
-      reject(new Error());
-      console.log('Got a response from the API');
-    }
-  }));
+export default function handleResponseFromAPI(promise) {
+  return promise
+    .then(() => ({ status: 200, body: 'success' }))
+    .catch(() => Error())
+    .finally(() => { console.log('Got a response from the API'); });
 }
