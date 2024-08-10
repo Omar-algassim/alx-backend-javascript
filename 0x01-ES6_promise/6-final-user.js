a8787c2e251d4dffeffdf61a7249aef209535c92
Omar-Algassim <omarggg2@gamil.com>
2024-08-11 02:05:43 +0300

    fix eslint

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,27 +2,24 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, filename) {
-  const SignupPromis = signUpUser(firstName, lastName);
-  const uploadPhotoPromis = uploadPhoto(filename);
-  return Promise.allSettled([SignupPromis, uploadPhotoPromis])
-    .then((value) => {
+  const signUpPromise = signUpUser(firstName, lastName);
+  const uploadPhotoPromise = uploadPhoto(filename);
+
+  return Promise.allSettled([signUpPromise, uploadPhotoPromise])
+    .then((values) => {
       const resultArray = [];
-      value.forEach((promise) => {
+      values.forEach((promise) => {
         if (promise.status === 'fulfilled') {
-          resultArray.push(
-            {
-              status: promise.status,
-              value: promise.value,
-            },
-          );
+          resultArray.push({
+            status: promise.status,
+            value: promise.value,
+          });
         }
         if (promise.status === 'rejected') {
-          resultArray.push(
-            {
-              status: promise.status,
-              value: `${promise.reason}`,
-            },
-          );
+          resultArray.push({
+            status: promise.status,
+            value: `${promise.reason}`,
+          });
         }
       });
       return resultArray;
