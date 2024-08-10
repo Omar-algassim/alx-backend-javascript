021a9b4f47de3856523bd528f18c771f9c3e2180
Omar-Algassim <omarggg2@gamil.com>
2024-08-11 00:21:01 +0300

    handleprofile sign in function

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,31 @@
+#!/usr/bin/node
+
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, filename) {
+  const SignupPromis = signUpUser(firstName, lastName);
+  const uploadPhotoPromis = uploadPhoto(filename);
+  return Promise.allSettled([SignupPromis, uploadPhotoPromis])
+    .then((value) => {
+      const resultArray = [];
+      value.forEach((promise) => {
+        if (promise.status === 'fulfilled') {
+          resultArray.push(
+            {
+              status: promise.status,
+              value: promise.value,
+            },
+          );
+        }
+        if (promise.status === 'rejected') {
+          resultArray.push(
+            {
+              status: promise.status,
+              value: promise.reason,
+            },
+          );
+        }
+      });
+    });
+}
