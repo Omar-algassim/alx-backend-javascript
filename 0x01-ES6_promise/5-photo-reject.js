9dddde3a8d201966751fa4ee9da72ccb79953def
Omar-Algassim <omarggg2@gamil.com>
2024-08-10 23:53:15 +0300

    return reject with error

--- 0x01-ES6_promise/5-photo-reject.js
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -1,9 +1,5 @@
 #!/usr/bin/node
 
 export default function uploadPhoto(filename) {
-  return new Promise((reject) => {
-    if (filename) {
-      reject(new Error(`${filename} cannot be processed`));
-    }
-  });
+  return Promise.reject(new Error(`${filename} cannot be processed`));
 }
