515ef5d79b0c53fb1fcd5cd99088cd43926c002d
Omar-Algassim <omarggg2@gamil.com>
2024-08-11 01:47:42 +0300

    implement simple try and catch

---
+++ 0x01-ES6_promise/9-try.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+export default function guardrail(mathFunction) {
+  const queue = [];
+  try {
+    queue.push(mathFunction());
+  } catch (e) {
+    queue.push(e.message);
+  }
+  queue.push('Guardrail was processed');
+  return queue;
+}
