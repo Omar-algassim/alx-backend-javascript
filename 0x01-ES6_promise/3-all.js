0528515093412ef577b00b11659fc882b917d7e9
Omar-Algassim <omarggg2@gamil.com>
2024-08-10 23:31:56 +0300

    return all prmises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -5,7 +5,7 @@ export default function handleProfileSignup() {
   const Uploadpromis = uploadPhoto();
   const CreateProms = createUser();
 
-  Promise.all([Uploadpromis, CreateProms])
+  return Promise.all([Uploadpromis, CreateProms])
     .then((value) => {
       console.log(`${value[0].body} ${value[1].firstName} ${value[1].lastName}`)
     })
