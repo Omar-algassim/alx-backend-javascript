e127624fbdf369ab9f98f27091211de9ce959602
Omar-Algassim <omarggg2@gamil.com>
2024-08-10 23:18:36 +0300

    eslint fix

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-export default function handleResponseFromAPI (promise) {
+export default function handleResponseFromAPI(promise) {
   return promise
     .then(() => ({ status: 200, body: 'success' }))
     .catch(() => Error())
