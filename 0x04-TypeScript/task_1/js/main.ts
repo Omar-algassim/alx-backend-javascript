41c797d12fcbc328429caa9f1b91af876f9380e8
Omar-Algassim <omarggg2@gamil.com>
2024-08-12 01:46:32 +0300

    typescript for director

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,7 +1,20 @@
-interface Teacer {
+interface Teacher {
     readonly firstName: string;
     readonly lastName: string;
     yearsOfExperience?: number;
     location: string;
     [key: string]: any;
 }
+
+interface Directors extends Teacher {
+    numberOfReports: number;
+}
+
+const director1: Directors = {
+    firstName: 'John',
+    lastName: 'Doe',
+    location: 'London',
+    fullTimeEmployee: true,
+    numberOfReports: 17,
+  };
+  console.log(director1);
