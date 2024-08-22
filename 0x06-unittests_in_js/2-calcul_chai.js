d4454155934f89cc56c34b0790891f6dea0cdef5
Omar-Algassim <omarggg2@gamil.com>
2024-08-22 23:49:09 +0300

    convert to common js module

--- 0x06-unittests_in_js/2-calcul_chai.js
+++ 0x06-unittests_in_js/2-calcul_chai.js
@@ -3,31 +3,36 @@
 When type is SUM, round the two numbers, and add a and b
 When type is SUBTRACT, round the two numbers, and subtract b from a
 When type is DIVIDE, round the two numbers, and divide a with b -
-if the rounded value of b is equal to 0, return the string Error
+if the const rounded value of b is equal to 0, return the string Error
 */
 
 function calculateNumber(type, a, b) {
-    const roundedA = Math.round(a);
-    const roundedB = Math.round(b);
     const calculate = {
         SUM: function (a, b) {
-            return a + b;
+            const roundedA = Math.round(a);
+            const roundedB = Math.round(b);
+            return roundedA + roundedB;
         },
         SUBTRACT: function(a, b) {
-            return a - b;
+            const roundedA = Math.round(a);
+            const roundedB = Math.round(b);
+            return roundedA - roundedB;
         },
         DIVIDE: function(a, b) {
-            if (b === 0) {
+            const roundedA = Math.round(a);
+            const roundedB = Math.round(b);
+            if (roundedB === 0) {
                 return "Error";
             }
-            return a / b;
+            return roundedA / roundedB;
         }
     }
     if (calculate[type] === undefined) {
         return 0;
     }
     const operation = calculate[type];
-    return operation(roundedA, roundedB);
+    return operation(a, b);
   }
   
-export default calculateNumber;
+  module.exports = calculateNumber;
+  
