c4a1606946f6c69f909cebeebd227955d8aea3ed
Omar-Algassim <omarggg2@gamil.com>
2024-08-22 20:37:22 +0300

    add rounded to zero test

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -8,43 +8,43 @@ describe('calculateNumber', function() {
     assert.equal(calculateNumber('SUM', 1.6, 1), 3);
   })
   it('test regular number added', function() {
-      assert.equal(calculateNumber('SUM', 1, 1), 2);
+    assert.equal(calculateNumber('SUM', 1, 1), 2);
   })
   it('rounded 0.5 and added', function() {
     assert.equal(calculateNumber('SUM', 1.5, 1.5), 4);
   })
   it('rounded under 0.5 and added', function() {
-      assert.equal(calculateNumber('SUM', 2.3, 1.1), 3);
+    assert.equal(calculateNumber('SUM', 2.3, 1.1), 3);
   })
   it('return the above 0.5 rounded and added', function() {
-      assert.equal(calculateNumber('SUM', 2.6, 1.7), 5);
+    assert.equal(calculateNumber('SUM', 2.6, 1.7), 5);
   })
   it('test the type is number', function() {
     const result = calculateNumber('SUM', 1.5, 2.5);
-      assert.ok(typeof(result), 'number')
+    assert.ok(typeof(result), 'number')
   })
 });
 
-    describe('#SUBTRUCT', function() {
-      it('shuold be rounded and subtruct', function() {
+  describe('#SUBTRUCT', function() {
+    it('shuold be rounded and subtruct', function() {
       assert.equal(calculateNumber('SUBTRACT', 1.6, 1), 1);
-      })
-      it('test regular number subtructing', function() {
-        assert.equal(calculateNumber('SUBTRACT', 1, 1), 0);
-      })
-      it('rounded 0.5 and subtruct', function() {
-        assert.equal(calculateNumber('SUBTRACT', 1.5, 1.5), 0);
-       })
-      it('rounded under 0.5 and subtruct', function() {
-        assert.equal(calculateNumber('SUBTRACT', 2.3, 3.1), -1);
-      })
-      it('return the above 0.5 rounded and subtruct', function() {
-        assert.equal(calculateNumber('SUBTRACT', 2.6, 1.7), 1);
-      })
-      it('test the type of number', function() {
+    })
+    it('test regular number subtructing', function() {
+      assert.equal(calculateNumber('SUBTRACT', 1, 1), 0);
+    })
+    it('rounded 0.5 and subtruct', function() {
+      assert.equal(calculateNumber('SUBTRACT', 1.5, 1.5), 0);
+    })
+    it('rounded under 0.5 and subtruct', function() {
+      assert.equal(calculateNumber('SUBTRACT', 2.3, 3.1), -1);
+    })
+    it('return the above 0.5 rounded and subtruct', function() {
+      assert.equal(calculateNumber('SUBTRACT', 2.6, 1.7), 1);
+    })
+    it('test the type of number', function() {
       const result = calculateNumber('SUBTRACT', 1.5, 2.5);
       assert.ok(typeof(result), 'number')
-      })
+    })
   });
   
   describe('#DIVISION', function() {
@@ -52,28 +52,30 @@ describe('calculateNumber', function() {
       assert.equal(calculateNumber('DIVIDE', 1.6, 1), 0.5);
     })
     it('test regular number divison', function() {
-        assert.equal(calculateNumber('DIVIDE', 1, 1), 1);
+      assert.equal(calculateNumber('DIVIDE', 1, 1), 1);
     })
     it('rounded 0.5 and divison', function() {
       assert.equal(calculateNumber('DIVIDE', 1.5, 5.6), 3);
     })
     it('rounded under 0.5 and divison', function() {
-        assert.equal(calculateNumber('DIVIDE', 2.3, 4), 2);
+      assert.equal(calculateNumber('DIVIDE', 2.3, 4), 2);
     })
     it('return the above 0.5 rounded and divison', function() {
-        assert.equal(calculateNumber('DIVIDE', 3.6, 1.7), 0.5);
+      assert.equal(calculateNumber('DIVIDE', 3.6, 1.7), 0.5);
     })
-    it('return the above 0.5 rounded and divison', function() {
-        assert.equal(calculateNumber('DIVIDE', 3.6, 0), 'Error');
+    it('test divison with zero', function() {
+      assert.equal(calculateNumber('DIVIDE', 3.6, 0), 'Error');
     })
-    it('test the type of number', function() {
+    it('test divison with rounded to zero', function() {
+      assert.equal(calculateNumber('DIVIDE', 3.6, 0.4), 'Error');
+    })
+    it('test the type is number', function() {
       const result = calculateNumber('DIVIDE', 1.5, 2.5);
-        assert.ok(typeof(result), 'number')
+      assert.ok(typeof(result), 'number')
     })
-    it('test the type of number', function() {
+    it('test the type of srting', function() {
       const result = calculateNumber('DIVIDE', 1.5, 0);
-        assert.ok(typeof(result), 'string')
+      assert.ok(typeof(result), 'string')
     })
   })
 });
-  
