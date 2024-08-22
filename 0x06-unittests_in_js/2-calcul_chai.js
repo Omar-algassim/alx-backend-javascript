f804e1f289c60f9520236d1fcc353ae9378887d2
Omar-Algassim <omarggg2@gamil.com>
2024-08-22 23:06:11 +0300

    update the test with chai

---
+++ 0x06-unittests_in_js/2-calcul_chai.js
@@ -0,0 +1,38 @@
+#!/usr/bin/node
+/*
+When type is SUM, round the two numbers, and add a and b
+When type is SUBTRACT, round the two numbers, and subtract b from a
+When type is DIVIDE, round the two numbers, and divide a with b -
+if the rounded value of b is equal to 0, return the string Error
+*/
+
+function calculateNumber(type, a, b) {
+    const calculate = {
+        SUM: function (a, b) {
+            roundedA = Math.round(a);
+            roundedB = Math.round(b);
+            return roundedA + roundedB;
+        },
+        SUBTRACT: function(a, b) {
+            roundedA = Math.round(a);
+            roundedB = Math.round(b);
+            return roundedA - roundedB;
+        },
+        DIVIDE: function(a, b) {
+            roundedA = Math.round(a);
+            roundedB = Math.round(b);
+            if (roundedB === 0) {
+                return "Error";
+            }
+            return roundedA / roundedB;
+        }
+    }
+    if (calculate[type] === undefined) {
+        return 0;
+    }
+    const operation = calculate[type];
+    return operation(a, b);
+  }
+  
+  module.exports = calculateNumber;
+  
---
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -0,0 +1,94 @@
+#!/usr/bin/node
+const chai = require('chai');
+const expect = chai.expect;
+const calculateNumber = require('./1-calcul.js')
+
+describe('calculateNumber', function() {
+  describe("#SUM", function () {
+    it('shuold be rounded and added', function() {
+    expect(calculateNumber('SUM', 1.6, 1), 3);
+  })
+  it('test regular number added', function() {
+    expect(calculateNumber('SUM', 1, 1)).to.equal(2);
+  })
+  it('rounded 0.5 and added', function() {
+    expect(calculateNumber('SUM', 1.5, 1.5)).to.equal(4);
+  })
+  it('rounded under 0.5 and added', function() {
+    expect(calculateNumber('SUM', 2.3, 1.1)).to.equal(3);
+  })
+  it('return the above 0.5 rounded and added', function() {
+    expect(calculateNumber('SUM', 2.6, 1.7)).to.equal(5);
+  })
+  it('test the type is number', function() {
+    const result = calculateNumber('SUM', 1.5, 2.5);
+    expect(typeof(result)).to.equal('number');
+  })
+});
+
+  describe('#SUBTRUCT', function() {
+    it('shuold be rounded and subtruct', function() {
+      expect(calculateNumber('SUBTRACT', 1.6, 1)).to.equal(1);
+    })
+    it('test negative number subtructing', function() {
+      expect(calculateNumber('SUBTRACT', -1, -1)).to.equal(0);
+    })
+    it('test negative number subtructing', function() {
+      expect(calculateNumber('SUBTRACT', 0, 1)).to.equal(-1);
+    })
+    it('rounded 0.5 and subtruct', function() {
+      expect(calculateNumber('SUBTRACT', 1.5, 1.5)).to.equal(0);
+    })
+    it('subtruct tow negative number should added', function() {
+      expect(calculateNumber('SUBTRACT', 3, -1.5)).to.equal(4);
+    })
+    it('rounded under 0.5 and subtruct', function() {
+      expect(calculateNumber('SUBTRACT', 2.3, 3.1)).to.equal(-1);
+    })
+    it('return the above 0.5 rounded and subtruct', function() {
+      expect(calculateNumber('SUBTRACT', 2.6, 1.7)).to.equal(1);
+    })
+    it('test the type of number', function() {
+      const result = calculateNumber('SUBTRACT', 1.5, 2.5);
+      expect(typeof(result).to.equal('number');
+    })
+  });
+
+  describe('#DIVISION', function() {
+    it('shuold be rounded and divison', function() {
+      expect(calculateNumber('DIVIDE', 1.6, 1)).to.equal(2);
+    })
+    it('test regular number divison', function() {
+      expect(calculateNumber('DIVIDE', 1, 1)).to.equal(1);
+    })
+    it('rounded 0.5 and divison negative number', function() {
+      expect(calculateNumber('DIVIDE', 5.6, -1.6)).to.equal(-3);
+    })
+    it('divide tow negative number', function() {
+      expect(calculateNumber('DIVIDE', -4, -2.3)).to.equal(2);
+    })
+    it('rounded under 0.5 and divison', function() {
+      expect(calculateNumber('DIVIDE', 4.3, 2.1)).to.equal(2);
+    })
+    it('return the above 0.5 rounded and divison', function() {
+      expect(calculateNumber('DIVIDE', 0.0, -5.0)).to.equal(0);
+    })
+    it('return the above 0.5 rounded and divison', function() {
+      expect(calculateNumber('DIVIDE', 0.0, 0.0)).to.equal('Error');
+    })
+    it('test divison with zero', function() {
+      expect(calculateNumber('DIVIDE', 3.6, 0)).to.equal('Error');
+    })
+    it('test divison with rounded to zero', function() {
+      expect(calculateNumber('DIVIDE', 3.6, 0.4)).to.equal('Error');
+    })
+    it('test the type is number', function() {
+      const result = calculateNumber('DIVIDE', 1.5, 2.5);
+      expect(typeof(result)).to.equal('number');
+    })
+    it('test the type of srting', function() {
+      const result = calculateNumber('DIVIDE', 1.5, 0);
+      expect(typeof(result)).to.equal('string');
+    })
+  })
+});
