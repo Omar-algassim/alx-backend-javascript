555d7a9dfd81262777fada1a177217cef60864f7
Omar-Algassim <omarggg2@gmail.com>
2024-09-18 16:00:01 +0300

    change content type to string

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,16 +1,14 @@
-#!/usr/bin/node
 const http = require('http');
 
-const hostName = '127.0.0.1';
+const host = '127.0.0.1';
 const port = 1245;
+
 const app = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader = {ContentType: 'text/plain'};
-  res.write('Hello Holberton School!');
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello Holberton School!');
 });
 
-app.listen(port, hostName, () => {
-    console.log(`Server running at http://${hostName}:${port}/`);
-});
+app.listen(port, host, () => {});
 
 module.exports = app;
