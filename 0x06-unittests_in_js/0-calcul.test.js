6a76dede33b3834582a58bb1d1f3d3a5702e525d
Omar-Algassim <omarggg2@gmail.com>
2024-09-20 14:45:05 +0300

    update the pacage test path and modify tests

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -5,11 +5,11 @@ describe('calculateNumber', () => {
     it('integer numbers', () => {
         assert.equal(calculateNumber(1, 3), 4);
     })
-    it('rounded number under 0.5', () => {
-        assert.equal(calculateNumber(1.3, 3.2), 4);
+    it('rounded number uo of 0.5 and integer', () => {
+        assert.equal(calculateNumber(1, 3.7), 5);
     })
-    it('rounded number up of 0.5', () => {
-        assert.equal(calculateNumber(1.6, 3.7), 6);
+    it('rounded number down of 0.5', () => {
+        assert.equal(calculateNumber(1.2, 3.4), 4);
     })
     it('add zeros', () => {
         assert.equal(calculateNumber(0, 0), 0);
